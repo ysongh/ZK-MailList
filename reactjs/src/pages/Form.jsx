3cc3d022fd3fa95c42f72e9ac6b8d4c03393e93c
You Song Hou <ysongweb3@gmail.com>
2024-02-12 01:15:31 -0500

    feat: update the UI for the form

--- reactjs/src/pages/Form.jsx
+++ reactjs/src/pages/Form.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { Input } from '../components/ui/input';
 import { Label } from '@radix-ui/react-label';
 import { Button } from '../components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
 
 function Form() {
   return (
-    <div>
-      <Label htmlFor="text">Email</Label>
-      <Input />
-      <Button>Click me</Button>
+    <div className="flex justify-center mt-10">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Subscribe</CardTitle>
+          <CardDescription>Enter your email</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="Email" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Add</Button>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
