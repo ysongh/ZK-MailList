c771f29f3bc40e3dbb4186744e8f8b186496ff43
You Song Hou <ysongweb3@gmail.com>
2024-02-13 22:50:28 -0500

    feat: integrate ConnectKit

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,4 +1,5 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Web3Provider } from "./Web3Provider";
 
 import Navbar from './components/layout/navbar';
 import { Button } from './components/ui/button';
@@ -8,21 +9,23 @@ import Form from './pages/Form';
 function App() {
 
   return (
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/test"
-          element={
-            <Button>Click me</Button>} />
-         <Route
-          path="/form"
-          element={<Form />} />
-        <Route
-          path="/"
-          element={<Landing /> } />
-      </Routes>
-    </HashRouter>
+    <Web3Provider>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/test"
+            element={
+              <Button>Click me</Button>} />
+          <Route
+            path="/form"
+            element={<Form />} />
+          <Route
+            path="/"
+            element={<Landing /> } />
+        </Routes>
+      </HashRouter>
+    </Web3Provider>
   )
 }
 
--- reactjs/src/pages/Landing.jsx
+++ reactjs/src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ConnectKitButton } from "connectkit";
 
 const Landing = () => {
   return (
@@ -6,9 +7,7 @@ const Landing = () => {
       <div className="max-w-md w-full bg-white shadow-md rounded-md p-8">
         <h1 className="text-3xl font-bold mb-4">Welcome to ZK MailList</h1>
         <p className="text-gray-600 mb-6">Streamline your email communication with ease.</p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Get Started
-        </button>
+        <ConnectKitButton />
       </div>
     </div>
   );
